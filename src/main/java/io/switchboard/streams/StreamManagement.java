3e96553d17487746c8270c0ab272c2f278ec9256
Christoph Grotz <christophgrotz@gmail.com>
2014-12-10 20:55:15 +0100

    Fixed Future resolution

--- src/main/java/io/switchboard/streams/StreamManagement.java
+++ src/main/java/io/switchboard/streams/StreamManagement.java
@@ -1,7 +1,11 @@
 package io.switchboard.streams;
 
+import akka.actor.AbstractActor;
 import akka.actor.UntypedActor;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
 import akka.http.server.japi.RouteResult;
+import akka.japi.pf.ReceiveBuilder;
 import com.google.common.collect.Lists;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,16 +13,16 @@ import org.slf4j.LoggerFactory;
 /**
  * Created by Christoph Grotz on 09.12.14.
  */
-public class StreamManagement extends UntypedActor {
+public class StreamManagement extends AbstractActor {
 
-  private static final Logger LOG = LoggerFactory.getLogger(StreamManagement.class);
+  private final LoggingAdapter log = Logging.getLogger(getContext().system(), this);
 
-  @Override
-  public void onReceive(Object message) throws Exception {
-    if(message instanceof RetrieveStreams) {
-      LOG.info("retrieve streams {}",message);
-      getSender().tell(Lists.newArrayList(new Streams("Test"),new Streams("Test2"),new Streams("Test3")), getSelf());
-    }
+  public StreamManagement() {
+    receive(ReceiveBuilder
+            .match(RetrieveStreams.class, message -> {
+              log.info("retrieve streams {}",message);
+              sender().tell(Lists.newArrayList(new Streams("Test"), new Streams("Test2"), new Streams("Test3")), self());
+            }).build());
   }
 
   public static class Streams implements RouteResult {
