da71a09c15b196e4f9440bfd3a57e0327127c50d
Christoph Grotz <christophgrotz@gmail.com>
2014-12-11 20:26:24 +0100

    Switched from gradle to maven; added antlr

---
+++ src/main/antlr4/io/switchboard/grammar/Switchboard.g4
@@ -0,0 +1,45 @@
+grammar Switchboard;
+
+statement : expr command* ;
+
+expr : expr ('AND' | 'OR' | 'NOT') expr # expopexp
+ | expr expr # expexp
+ | predicate # predicexpr
+ | text # textexpr
+ | '(' expr ')' # exprgroup
+ ;
+
+predicate : text ('=' | '!=' | '>=' | '<=' | '>' | '<') text ;
+
+command : '| show' text* # showcmd
+ | '| show' text (',' text)* # showcsv
+ ;
+
+text : NUMBER # numbertxt
+ | QTEXT # quotedtxt
+ | UQTEXT # unquotedtxt
+ ;
+
+AND : 'AND' ;
+OR : 'OR' ;
+NOT : 'NOT' ;
+EQUALS : '=' ;
+NOTEQUALS : '!=' ;
+GREQUALS : '>=' ;
+LSEQUALS : '<=' ;
+GREATERTHAN : '>' ;
+LESSTHAN : '<' ;
+
+NUMBER : DIGIT+
+ | DIGIT+ '.' DIGIT+
+ | '.' DIGIT+
+ ;
+QTEXT : '"' (ESC|.)*? '"' ;
+UQTEXT : ~[ ()=,<>!\r\n]+ ;
+
+fragment
+DIGIT : [0-9] ;
+fragment
+ESC : '\\"' | '\\\\' ;
+
+WS : [ \t\r\n]+ -> skip ;
--- src/main/java/io/switchboard/Boot.java
+++ src/main/java/io/switchboard/Boot.java
@@ -2,6 +2,9 @@ package io.switchboard;
 
 import akka.actor.ActorSystem;
 import io.switchboard.api.BasicApi;
+import io.switchboard.grammar.SwitchboardLexer;
+import io.switchboard.grammar.SwitchboardParser;
+import org.antlr.v4.runtime.*;
 
 /**
  * Main class for switchboard
@@ -13,6 +16,20 @@ public class Boot{
   public static void main(String ... args) throws Exception {
     ActorSystem actorSystem = ActorSystem.create();
     BasicApi.apply(actorSystem).bindRoute("0.0.0.0", 8080);
+
+    //System.out.println(parse("Abishek AND (country=India OR city=NY) LOGIN 404 | show name city").expr().getChild(0).getChild(0));
+  }
+
+  private static SwitchboardParser.StatementContext parse(String input) {
+    SwitchboardLexer l = new SwitchboardLexer(new ANTLRInputStream(input));
+    SwitchboardParser p = new SwitchboardParser(new CommonTokenStream(l));
+    p.addErrorListener(new BaseErrorListener() {
+      @Override
+      public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
+        throw new IllegalStateException("failed to parse at line " + line + " due to " + msg, e);
+      }
+    });
+    return p.statement();
   }
 
 }
