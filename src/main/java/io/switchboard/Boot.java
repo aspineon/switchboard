663fa16de0819502eedc2fcf5b00c5b348c38750
Christoph Grotz <christophgrotz@gmail.com>
2014-12-19 22:20:14 +0100

    updated to kafka 0.8.2-beta

--- src/main/java/io/switchboard/Boot.java
+++ src/main/java/io/switchboard/Boot.java
@@ -1,57 +1,74 @@
 package io.switchboard;
 
 import akka.actor.ActorSystem;
-import akka.japi.JavaPartialFunction;
 import akka.stream.FlowMaterializer;
 import akka.stream.javadsl.Sink;
 import akka.stream.javadsl.Source;
-import akka.stream.javadsl.japi.Function;
-import akka.stream.javadsl.japi.Procedure;
-import akka.util.ByteString$;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.collect.Lists;
+import io.switchboard.api.BasicApi;
 import io.switchboard.kafka.KafkaSubscriber;
 import io.switchboard.processing.Switchboard;
-import scala.concurrent.duration.FiniteDuration;
-
-import java.util.List;
-import java.util.concurrent.TimeUnit;
+import joptsimple.internal.Strings;
+import org.apache.commons.cli.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Main class for switchboard
  *
  * Created by Christoph Grotz on 18.11.14.
  */
-public class Boot{
+public class Boot {
+  private static final Logger LOG = LoggerFactory.getLogger(Boot.class);
 
   public static void main(String ... args) throws Exception {
-    ObjectMapper mapper = new ObjectMapper();
-    ActorSystem actorSystem = ActorSystem.create();
-    //BasicApi.apply(actorSystem).bindRoute("0.0.0.0", 8080);
+    try {
+      Options options = new Options();
+      options.addOption("i", true, "interface");
+      options.addOption("p", true, "port");
 
+      PosixParser parser = new PosixParser();
+      CommandLine cmd = parser.parse(options, args);
 
-    //Source<ObjectNode> source = Source.from(new FiniteDuration(1, TimeUnit.SECONDS),new FiniteDuration(1, TimeUnit.SECONDS), () -> node);
+      String host;
+      if (cmd.hasOption("i")) {
+        host = cmd.getOptionValue("i");
+      } else if (!Strings.isNullOrEmpty(System.getenv("SWITCHBOARD_HOST_INTERFACE"))) {
+        host = System.getenv("SWITCHBOARD_HOST_INTERFACE");
+      } else {
+        host = "0.0.0.0";
+      }
 
-    /*Switchboard
-      .expression("switchboard | type=request AND (country=India OR city=NY)")
-      .runWith(FlowMaterializer.create(actorSystem), source, sink);*/
+      String portStr;
+      if (cmd.hasOption("p")) {
+        portStr = cmd.getOptionValue("p");
+      } else if (!Strings.isNullOrEmpty(System.getenv("SWITCHBOARD_HOST_PORT"))) {
+        portStr = System.getenv("SWITCHBOARD_HOST_PORT");
+      } else {
+        portStr = "8080";
+      }
 
-    Switchboard
-      .expression("FROM switchboard | type=request AND country=India OR city=NY AND value.numeric = 1 | TO topic2")
-      .runWithKafka(FlowMaterializer.create(actorSystem),
-        "adhoc-group-"+System.currentTimeMillis());
+      ActorSystem actorSystem = ActorSystem.create();
+      BasicApi.apply(actorSystem).bindRoute(host, Integer.parseInt(portStr));
 
-    Source.from(Lists.newArrayList(
-      "{\"type\":\"request\",\"country\":\"India\",\"value\": { \"numeric\": 1}}",
-      "{\"type\":\"request\",\"country\":\"Cananda\",\"value\": { \"numeric\": 1}}",
-      "{\"type\":\"request\",\"country\":\"Pakistan\",\"value\": { \"numeric\": 1}}",
-      "{\"type\":\"request\",\"country\":\"US\",\"city\":\"NY\",\"value\": { \"numeric\": 1}}",
-      "{\"type\":\"request\",\"country\":\"US\",\"city\":\"LA\",\"value\": { \"numeric\": 1}}"
-    ))
-      //.map( param -> mapper.readValue(param, ObjectNode.class))
-      .runWith(Sink.create(new KafkaSubscriber("switchboard")), FlowMaterializer.create(actorSystem));
+      /*
+      Switchboard
+        .expression("FROM switchboard | type=request AND country=India OR city=NY AND value.numeric = 1 | TO topic2")
+        .runWithKafka(FlowMaterializer.create(actorSystem),
+          "adhoc-group-" + System.currentTimeMillis());
+
+      Source.from(Lists.newArrayList(
+        "{\"type\":\"request\",\"country\":\"India\",\"value\": { \"numeric\": 1}}",
+        "{\"type\":\"request\",\"country\":\"Cananda\",\"value\": { \"numeric\": 1}}",
+        "{\"type\":\"request\",\"country\":\"Pakistan\",\"value\": { \"numeric\": 1}}",
+        "{\"type\":\"request\",\"country\":\"US\",\"city\":\"NY\",\"value\": { \"numeric\": 1}}",
+        "{\"type\":\"request\",\"country\":\"US\",\"city\":\"LA\",\"value\": { \"numeric\": 1}}"
+      )).runWith(Sink.create(new KafkaSubscriber("switchboard")), FlowMaterializer.create(actorSystem));
+      */
+    }
+    catch(ParseException exp) {
+      LOG.error("Error while parsing arguments", exp);
+    }
   }
 
 }
